c3b0986c8cec4503cd7545d8933463ce8e965773
NellyGilA <86023328+NellyGilA@users.noreply.github.com>
2025-02-24 21:25:00 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,9 @@
-from flask import Flask, render_template, url_for, request, jsonify
-from text_sentiment_prediction import *
 
-app = Flask(__name__)
-@app.route('/')
-def index():
-    return render_template('index.html')
- 
-@app.route('/predict-emotion', methods=["POST"])
-def predict_emotion():
     
     # Obtener el texto ingresado del requerimiento POST.
    
     
-    if not input_text:
+    
         # Respuesta para enviar si input_text está indefinido.
        
         
@@ -21,7 +12,7 @@ def predict_emotion():
         # Enviar respuesta.         
         
        
-app.run(debug=True)
+
 
 
 
